2f495f94161c3cdc23ee748d7785206cba8da23c
Eldar <elogon@gmail.com>
2020-12-17 09:28:55 -0500

    Establish connection to DB

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const mysql = require('mysql');
+const inquirer = require ('inquirer');
+const mysql = require ('mysql');
+const cTable = require('console.table');
+const dotenv = require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: 'root',
+    // Be sure to update with your own MySQL password!
+    password: process.env.SECRET_KEY,
+    database: 'company_db',
+  });
+
+  // Connect to the DB
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\n`);
+
+    connection.end();
+  });
